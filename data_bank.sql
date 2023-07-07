fe7be790338ba9d103ccf793e2745884df912424
Kamil <131042937+xExuberantx@users.noreply.github.com>
2023-07-07 11:52:56 +0200

    Update data_bank.sql

--- data_bank.sql
+++ data_bank.sql
@@ -9409,7 +9409,34 @@ VALUES
   ('189', '2020-01-22', 'deposit', '302'),
   ('189', '2020-01-27', 'withdrawal', '861');
 
-/*markdown
-# YO mama
-### Hiho
-*/
+-- A. Customer Nodes Exploration
+
+-- 1. How many unique nodes are there on the Data Bank system?
+
+-- 2. What is the number of nodes per region?
+
+-- 3. How many customers are allocated to each region?
+
+-- 4. How many days on average are customers reallocated to a different node?
+
+-- 5. What is the median, 80th and 95th percentile for this same reallocation days metric for each region?
+
+-- B. Customer Transactions
+
+-- 1. What is the unique count and total amount for each transaction type?
+
+-- 2. What is the average total historical deposit counts and amounts for all customers?
+
+-- 3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+
+-- 4. What is the closing balance for each customer at the end of the month?
+
+-- 5. What is the percentage of customers who increase their closing balance by more than 5%?
+
+-- C. Data Allocation Challenge
+
+-- Option 1: data is allocated based off the amount of money at the end of the previous month
+
+-- Option 2: data is allocated on the average amount of money kept in the account in the previous 30 days
+
+-- Option 3: data is updated real-time
